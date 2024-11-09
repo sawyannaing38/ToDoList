8092d7f6d5eaeb202a74f78bf9b4c2ea40410fc8
Saw Yan Naing <sawyan820@gmail.com>
2024-11-09 11:31:43 +0630

    Adding total list item features

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 "use strict";
 
 // Importing
-import { openMenu, closeMenu } from "./common.js";
+import { openMenu, closeMenu, getHomeListItem, showNoListsMessage } from "./common.js";
 
 // Selecting Dom
 const openMenuBtn = document.querySelector(".menu");
@@ -61,7 +61,7 @@ document.addEventListener("DOMContentLoaded", () => {
                     return;
                 }
                 // Create an item
-                const item = getListItem(todayList);
+                const item = getHomeListItem(todayList);
                 listItems.append(item);
 
                 // Adding event listener on more Btn
@@ -100,70 +100,6 @@ document.addEventListener("DOMContentLoaded", () => {
         showNoListsMessage();
     }
 })
-
-// For Getting to do list item
-function getListItem(obj)
-{
-    // Create a div with item class
-    const div = document.createElement("div");
-    div.className = "item";
-    div.innerHTML = `<span class="hidden">${obj.importance}</span>`
-    // Create another div with details class name
-    const detailDiv = document.createElement("div");
-    detailDiv.className = "details";
-
-    // Create another div with importance class name
-    const importanceDiv = document.createElement("div");
-    importanceDiv.className = "importance";
-
-    // Loop the important of obj
-    for (let i = 0; i < obj.importance; i++)
-    {
-        importanceDiv.innerHTML += `
-            <span class="material-symbols-outlined">
-                grade
-            </span>
-        `;
-    }
-
-    // Appedning divTwo to item div
-    detailDiv.append(importanceDiv);
-
-    detailDiv.innerHTML += `
-        <h2 class="item-title">${obj.title}</h2>
-        <span class="status-${obj.status}">${obj.status}</span>
-    `;
-
-    div.append(detailDiv);
-
-    if (obj.status == "unfinished")
-    {
-        div.innerHTML += `
-        <div class="control">
-            <span class="material-symbols-outlined more-btn">
-                more_vert
-            </span>
-            <ul class="control-buttons">
-                <li class="success">Success</li>
-                <li class="remove">Remove</li>
-            </ul>
-        </div>
-    `;
-    }
-
-    return div;
-
-}
-
-// For showing no to do lists
-function showNoListsMessage()
-{
-    const p =document.createElement("p");
-    p.className = "remind";
-    p.innerHTML = `You don't have any to do lists.Add <a href="add.html">Here.</a>`;
-
-    listItems.append(p);
-}
 
 // For Success Progress
 function successProgress(obj)
---
+++ js/total.js
@@ -0,0 +1,38 @@
+import { openMenu, closeMenu, showNoListsMessage, getTotalListItem } from "./common.js";
+
+const openMenuBtn = document.querySelector(".menu");
+const closeMenuBtn = document.querySelector(".menu-close");
+const totalList = document.querySelector(".total-list");
+const listItems = document.querySelector(".list-items")
+
+openMenuBtn.addEventListener("click", openMenu);
+closeMenuBtn.addEventListener("click", closeMenu);
+
+document.addEventListener("DOMContentLoaded", () => {
+    // Get the total lists
+    const todoLists = JSON.parse(localStorage.getItem("todolists"));
+
+    if (todoLists)
+    {
+        // Get the total lists
+        const totalLists = todoLists.length;
+
+        // show total lists
+        totalList.textContent = `${totalLists} items`;
+
+        // Loop it
+        for (const todoList of todoLists)
+        {
+            // Getting item
+            const item = getTotalListItem(todoList);
+
+            listItems.append(item);
+        }
+    }
+
+    else 
+    {
+        // Show no todolists
+        showNoListsMessage();
+    }
+})
